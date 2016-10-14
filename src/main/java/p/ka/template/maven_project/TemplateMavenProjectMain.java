2e7de874a566cec7be0e97643f7782af48313055
xiaochangyu <xiaocy@cstonline.com>
2016-10-14 15:39:10 +0800

    Maven Project Template. build jar. manifest

---
+++ src/main/java/p/ka/template/maven_project/TemplateMavenProjectMain.java
@@ -0,0 +1,25 @@
+package p.ka.template.maven_project;
+
+/**
+ * 功能描述: 模板 Maven-Project 主入口类
+ * @createTime: 2016年10月12日 下午7:07:24
+ * @author: <a href="mailto:676096658@qq.com">xiaochangyu</a>
+ * @version: 0.1
+ * @lastVersion: 0.1
+ * @updateTime: 2016年10月12日 下午7:07:24
+ * @updateAuthor: <a href="mailto:676096658@qq.com">xiaochangyu</a>
+ * @changesSum:
+ */
+public class TemplateMavenProjectMain {
+
+	/**
+	 * 功能描述: 程序入口
+	 * @createTime: 2016年10月12日 下午7:07:56
+	 * @author: <a href="mailto:676096658@qq.com">xiaochangyu</a>
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Congratulation! Run Right!");
+		System.out.println("恭喜! 运行正确!");
+	}
+}
